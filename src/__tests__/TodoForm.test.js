a09eba8d209fd05db455a10724746d335c461c4d
goughjo02 <goughjo02@gmail.com>
2021-10-01 12:02:14 +0100

    fill out todo form

--- src/__tests__/TodoForm.test.js
+++ src/__tests__/TodoForm.test.js
@@ -1,6 +1,7 @@
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 import React from "react";
-import {render, fireEvent, screen} from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Store from "../context";
 import TodoForm from "../components/TodoForm";
@@ -8,11 +9,21 @@ import TodoForm from "../components/TodoForm";
 test("<TodoForm /> #addTodo", async () => {
   const dispatch = jest.fn();
 
-  render( 
+  render(
     <Store.Provider value={{ dispatch }}>
       <TodoForm />
     </Store.Provider>
-  )
+  );
+  const newTodo = "some new todo";
 
-  // spy and asserts here
+  const textInput = screen.getByTestId("input");
+  const addButton = screen.getByText("Add");
+
+  userEvent.type(textInput, newTodo);
+  userEvent.click(addButton);
+
+  expect(dispatch).toBeCalledWith({
+    type: "ADD_TODO",
+    payload: newTodo
+  });
 });
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,21 +1,47 @@
 import React from "react";
+import { useState } from "react/cjs/react.development";
+import { useStore } from "../context";
 
 const TodoForm = () => {
+  const [value, setValue] = useState("");
+  const { dispatch } = useStore();
+  const handleInputChange = (event) => {
+    const { value: newValue } = event.target;
+    setValue(newValue);
+  };
+  const handleSubmit = () => {
+    dispatch({
+      type: "ADD_TODO",
+      payload: value
+    });
+  };
   return (
     <div className="row">
       <div className="col-md-12">
         <br />
         <div className="input-group">
-            {/* input here */}
-            {/* use for input class 'form-control' and attribute data-testid='input' */}
+          <label htmlFor="add-todo">Enter new todo</label>
+          <input
+            id="add-todo"
+            type="text"
+            className="form-control"
+            data-testid="input"
+            value={value}
+            onChange={handleInputChange}
+          />
           <div className="input-group-append">
-            {/* button here */}
-            {/* use for button class `btn btn-primary` and text `Add` */}
+            <button
+              className="btn btn-primary"
+              type="button"
+              onClick={handleSubmit}
+            >
+              Add
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default TodoForm
+export default TodoForm;
