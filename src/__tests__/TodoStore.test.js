ee1b4f7d23bf4cc6aae0085707eaef4f1321be50
goughjo02 <goughjo02@gmail.com>
2021-10-01 12:02:02 +0100

    write test for reducer

--- src/__tests__/TodoStore.test.js
+++ src/__tests__/TodoStore.test.js
@@ -1,9 +1,19 @@
 import reducer from "../reducer";
 
 test("adds todo", async () => {
- // asserts here
+  const initialState = {
+    todos: ["a", "b"]
+  };
+  const newState = reducer(initialState, { type: "ADD_TODO", payload: "c" });
+  const expectedState = { todos: ["a", "b", "c"] };
+  expect(newState).toEqual(expectedState);
 });
 
 test("completes todo", async () => {
- //  asserts here
+  const initialState = {
+    todos: ["a", "b", "c"]
+  };
+  const newState = reducer(initialState, { type: "COMPLETE", payload: "b" });
+  const expectedState = { todos: ["a", "c"] };
+  expect(newState).toEqual(expectedState);
 });
